2ea60df4d53b8ed6bffd2f2939a8cdfc165176c9
ki-opt <rtkdm708@gmail.com>
2023-02-05 00:11:43 +0900

    for AHC17

--- AHC17/src/src.cpp
+++ AHC17/src/src.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 
 using std::vector; using std::cout; using std::cin;
-using std::string;
+using std::string; using std::mt19937; using std::uniform_int_distribution;
+
+#define CONTEST false
 
 class Instance {
 // メンバ関数
@@ -14,7 +17,17 @@ public:
 
 // コンストラクタ
 public:
-   //instance_read(string filename) { }
+   Instance(string filename) { 
+      FILE *fpFile;
+      fpFile = fopen(filename.c_str(),"r");
+      if (fpFile == NULL) { cout << "cannot read instance"; getchar(); }
+      fscanf(fpFile,"%d %d %d %d", &N, &M, &D, &K);
+      this->edge_weight.assign(M, vector<int>(3));
+      this->node_coord.assign(N, vector<int>(2));
+      for (int i = 0; i < M; i++) { for (int j = 0; j < 3; j++) fscanf(fpFile, "%d", &edge_weight[i][j]); }
+      for (int i = 0; i < N; i++) { for (int j = 0; j < 2; j++) fscanf(fpFile, "%d", &node_coord[i][j]); }
+      fclose(fpFile);
+   }
    Instance() {
       cin >> this->N; cin >> this->M; cin >> this->D; cin >> this->K;
       this->edge_weight.assign(M, vector<int>(3));
@@ -24,17 +37,69 @@ public:
    };
 };
 
+class RandomNumber {
+public:
+   mt19937 rnd;
+   RandomNumber(int seed) {
+      this->rnd.seed(seed);
+   }
+   int random_distribution(int min_val, int max_val) {
+      uniform_int_distribution<> dist(min_val, max_val);
+      return dist(rnd);
+   }
+};
+
 class Solution {
 public:
    vector<int> sol;
+   vector<int> kozi_count;
+public:
+   Solution(const Instance &inst) { 
+      sol.resize(inst.M);
+      kozi_count.assign(inst.D,0);
+   }
+public:
+   void create_random_init_sol(const Instance &inst, RandomNumber &rnd) {
+      for (int i = 0; i < inst.M; i++) {
+         int random_number = rnd.random_distribution(1,inst.D);
+         while (kozi_count[random_number - 1] > inst.K) random_number = rnd.random_distribution(1,inst.D);
+         sol[i] = random_number;
+         kozi_count[random_number - 1]++;
+      }
+   }
+   void output(const Instance &inst) {
+      for (int i = 0; i < inst.M - 1; i++) cout << sol[i] << " ";
+      cout << sol[sol.size() - 1];
+   }
+   void output_txtfile(const Instance &inst) {
+      FILE *fpWrite;
+      string fnameWrite = "result.txt";
+      fpWrite = fopen(fnameWrite.c_str(),"w");
+      if (fpWrite == NULL) { cout << "cannot output result"; getchar(); }
+      for (int i = 0; i < inst.M - 1; i++) fprintf(fpWrite, "%d ", sol[i]);
+      fprintf(fpWrite, "%d ", sol[sol.size()-1]);
+      fclose(fpWrite);
+   }
 };
 
-
-
 int main() {
-   Instance inst();
-   Solution solution;
+#if CONTEST
+   Instance inst;                            // for AHC
+#else
+   Instance inst("inst/inst_0.txt");         // for test
+#endif
+   Solution solution(inst);
+   RandomNumber rnd(0);
 
+   // 初期解の生成
+   solution.create_random_init_sol(inst, rnd);
+
+   // 解の出力
+#if CONTEST
+   solution.output(inst);
+#else
+   solution.output_txtfile(inst);
+#endif
 
    return 0;
 }
