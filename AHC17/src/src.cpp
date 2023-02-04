a912e0ea956d1bb483e79641f24736197ec76373
ki-opt <rtkdm708@gmail.com>
2023-02-04 20:20:48 +0900

    modify

--- AHC17/src/src.cpp
+++ AHC17/src/src.cpp
@@ -3,11 +3,15 @@
 
 using std::vector; using std::cout; using std::cin;
 
-void func() {
+class instance {
 
-}
+
+};
 
 int main() {
+   vector<int> vec;
+   cout << vec[0];
    cout << 1;
-   vector<int> aaa;
+   cout << 2;
+   return 0;
 }
