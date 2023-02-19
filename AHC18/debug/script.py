92008540eba1b765959c44809ceeeea5739c4f7f
ki-opt <rtkdm708@gmail.com>
2023-02-20 08:15:20 +0900

    AHC18

--- AHC18/debug/script.py
+++ AHC18/debug/script.py
@@ -1,10 +1,11 @@
 import subprocess
 from subprocess import PIPE
 import re
+import pandas as pd
 
 SEED = [i for i in range(0,100)]#10)]
 
-
+cost = pd.DataFrame(index=SEED,columns=['cost','ave','std'])
 
 for seed in SEED:
    if seed < 10: filename = f'000{seed}'
@@ -13,7 +14,10 @@ for seed in SEED:
    else: input('error')
    res = subprocess.run(f'powershell -Command cat tester/inst/{filename}.txt | tester/./tester.exe ./out.exe > tester/res/{filename}.txt',\
       stderr=PIPE)
-   #if seed == 5: print(res)
    total_cost = res.stderr.decode('utf-8')
    total_cost = int(re.sub(r'\D','',total_cost))
+   cost.loc[seed,'cost'] = total_cost
    print(f'seed {filename}: ',total_cost)
+
+cost.loc[SEED[0],['ave','std']] = cost['cost'].mean(), cost['cost'].std()
+cost.to_csv('cost.csv')
